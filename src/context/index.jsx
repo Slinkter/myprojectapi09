b54f6c93ff1529b4771729198167ad562b9047c2
Slinkter <luis.j.cueva@gmail.com>
2024-03-20 23:01:44 -0500

    ok

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const GlobalContext = createContext(null);
 
 const GlobalState = () => {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([]);
+
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {}
+  function handleAddToFavorite(getCurrentItem) {}
+
   return <div>GlobalState</div>;
 };
 
